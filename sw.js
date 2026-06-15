7a5f48ae662f278081f63bf9a61121f682b2adb7
renteria00 <renteriafacturas@hotmail.com>
2026-06-14 23:11:38 -0600

    Subida inicial

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'rors-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg', './data.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const clone = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
